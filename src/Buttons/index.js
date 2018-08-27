14607f0d36feb3292718a7cec1ed3f62c8e91f29
mdashmiller <m.robert.miller@gmail.com>
2018-08-27 14:14:07 -0500

    set color palette and media queries

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -5,33 +5,33 @@ const ButtonPanel = ({ handleChange, calculate }) =>
 	// sends values from button to App state
 	<div className="wrapper">
 		<div className="top-row">
-			<input className="button" type="button" value="ce" onClick={handleChange} />
-			<input className="button" type="button" value="c" onClick={handleChange} />
-			<input className="button" type="button" value="+/-" onClick={() => calculate()} />
+			<input className="button func" type="button" value="ce" onClick={handleChange} />
+			<input className="button func" type="button" value="c" onClick={handleChange} />
+			<input className="button mod" type="button" value="+/-" onClick={handleChange} />
 		</div>
-		<div>
-			<input className="button" type="button" value="7" onClick={handleChange} />
-			<input className="button" type="button" value="8" onClick={handleChange} />
-			<input className="button" type="button" value="9" onClick={handleChange} />
-			<input className="button" type="button" value="/" onClick={handleChange} />
+		<div className="button-row">
+			<input className="button number" type="button" value="7" onClick={handleChange} />
+			<input className="button number" type="button" value="8" onClick={handleChange} />
+			<input className="button number" type="button" value="9" onClick={handleChange} />
+			<input className="button op" type="button" value="/" onClick={handleChange} />
 		</div>
-		<div>
-			<input className="button" type="button" value="4" onClick={handleChange} />
-			<input className="button" type="button" value="5" onClick={handleChange} />
-			<input className="button" type="button" value="6" onClick={handleChange} />
-			<input className="button" type="button" value="*" onClick={handleChange} />
+		<div className="button-row">
+			<input className="button number" type="button" value="4" onClick={handleChange} />
+			<input className="button number" type="button" value="5" onClick={handleChange} />
+			<input className="button number" type="button" value="6" onClick={handleChange} />
+			<input className="button op" type="button" value="*" onClick={handleChange} />
 		</div>
-		<div>
-			<input className="button" type="button" value="1" onClick={handleChange} />
-			<input className="button" type="button" value="2" onClick={handleChange} />
-			<input className="button" type="button" value="3" onClick={handleChange} />
-			<input className="button" type="button" value="-" onClick={handleChange} />
+		<div className="button-row">
+			<input className="button number" type="button" value="1" onClick={handleChange} />
+			<input className="button number" type="button" value="2" onClick={handleChange} />
+			<input className="button number" type="button" value="3" onClick={handleChange} />
+			<input className="button op" type="button" value="-" onClick={handleChange} />
 		</div>
-		<div>
-			<input className="button" type="button" value="0" onClick={handleChange} />
-			<input className="button" type="button" value="." onClick={handleChange} />
-			<input className="button" type="button" value="=" onClick={handleChange} />
-			<input className="button" type="button" value="+" onClick={handleChange} />
+		<div className="button-row">
+			<input className="button number" type="button" value="0" onClick={handleChange} />
+			<input className="button mod" type="button" value="." onClick={handleChange} />
+			<input className="button func" type="button" value="=" onClick={() => calculate()} />
+			<input className="button op" type="button" value="+" onClick={handleChange} />
 		</div>
 	</div>	
 
