892c2e5f282c61b3f1819d0ddfc154baaf8c09a9
mdashmiller <m.robert.miller@gmail.com>
2018-12-07 13:38:53 -0600

    fixed ce with negative terms

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import '../index.css'
 import PropTypes from 'prop-types'
 
 const ButtonPanel = ({ handleClick, handleKeyDown, calculate }) => 
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ButtonPanel from './ButtonPanel'
-// import '../index.css'
 import PropTypes from 'prop-types'
 
 const Calculator = ({
