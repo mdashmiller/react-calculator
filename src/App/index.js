97a89432d1e006cc84cfd8b3c26ffe939bb77cfe
mdashmiller <m.robert.miller@gmail.com>
2018-12-13 18:24:23 -0600

    removed extra spaces from negative terms

--- src/App/index.js
+++ src/App/index.js
@@ -235,7 +235,7 @@ class App extends Component {
 			// message to the store
 			if (display === 'LIMIT EXCEEDED') return
 
-			const newStore = Utils.charStrCombiner(operator, store, display)// '+'  // '1 + ' // '-2'
+			const newStore = Utils.charStrCombiner(operator, store, display)
 
 			return {
 				store: newStore,
@@ -452,7 +452,6 @@ class App extends Component {
 		})
 	
 	render() {
-		// console.log(`runningTotal: ${this.state.runningTotal}`)
 		return (
 			<div className="app">
 				<Calculator 
--- src/functions/PosNeg.js
+++ src/functions/PosNeg.js
@@ -4,28 +4,18 @@ const ops = ['+', '-', 'x', '/']
 
 const PosNeg = {
 
-	handleParens: function (str) { // '1 + -2 +'
+	handleParens: function (str) {
 		// takes a string and encapsulates all
 		// its negative terms in parentheses
-		const openParens = this.addOpenParens(str) // '1 + (-2 +'
-		const openParensArr = [...openParens]
-		const beginCheckLocations = []
+		const openParensStr = this.addOpenParens(str)
+		const openParensArr = [...openParensStr]
+		const beginCheckLocations = this.findCheckLocations(openParensArr)
+		const storePrefix = this.createStorePrefix(beginCheckLocations, openParensStr)
 
-		// finds each location to begin checking for
-		// where to insert a closing parenthesis
-		openParensArr.forEach((item, index) => {
-			if (item === '(') {
-				beginCheckLocations.push(index + 4) // [8]
-				// beginCheckLocations.push(index + 2) // [6]
-			}						 
-		})
-
-		const storePrefix = this.createStorePrefix(beginCheckLocations, openParens) // [8] // '1 + (-2 +'
-
-		return this.createCheckStrings(beginCheckLocations, openParens, storePrefix) // [8] // '1 + (-2 +' // '1 + (-2 '
+		return this.createCheckStrings(beginCheckLocations, openParensStr, storePrefix)
 	},
 
-	addOpenParens: str => { // '1 + -2 +'
+	addOpenParens: str => {
 		// takes a string and inserts an opening parenthesis 
 		// in front of each negative number
 		const dblNeg = /- -/g
@@ -36,28 +26,61 @@ const PosNeg = {
 
 		// remove any pre-existing closing parentheses
 		// to avoid double parentheses later
-		const baseString = str.indexOf(')') !== -1
+		const baseString = str.indexOf(')') !== -1 
 			? str.replace(endParen, '')
 			: str
 
 		// insert opening parentheses where needed
 		let openParens = baseString.replace(dblNeg, '- (-')
-		openParens = openParens.replace(plusNeg, '+ (-') // '1 + (-2 +'
+		openParens = openParens.replace(plusNeg, '+ (-')
 		openParens = openParens.replace(timesNeg, 'x (-')
 		return openParens.replace(divNeg, '/ (-')
 	},
 
-	createStorePrefix: (locationsArr, str) => { // [8] // '1 + (-2 +'
+	findCheckLocations: arr => {
+		// find the index of each location from which
+		// to begin looking for the location to 
+		// insert a closing parenthesis
+		const beginCheckLocations = []
+
+		let negSignIndex
+		let subArray
+		let nextOpInSubArray
+		let beginCheckLoc
+
+		arr.forEach((item, index) => {
+			if (item === '(') {
+				// find the location of the negative sign 
+				// for each negative term
+				negSignIndex = index + 1
+				// create a sub-array of everything that comes after
+				// the '-' of a negative term each time one is found
+				subArray = arr.filter((item, index) => index > negSignIndex)
+				// identify the next operator that comes after the
+				// '-' of each negative term
+				nextOpInSubArray = Utils.firstOperator(subArray)
+				// create a check location one index position before 
+				// the operator that directly follows the '-' of
+				// each negative term
+				beginCheckLoc = arr.indexOf(nextOpInSubArray, negSignIndex + 1) - 1
+				beginCheckLocations.push(beginCheckLoc)
+			}						 
+		})
+
+		return beginCheckLocations
+	},
+
+	createStorePrefix: (locationsArr, str) => {
 		// removes and saves the portion of 
 		// the string that occurs before the first
 		// possible location of a closing parenthesis
-		const cutoff = locationsArr[0] // 8
+		const cutoff = locationsArr[0]
 		
-		return [...str].filter((item, index) => index < cutoff) // index < 7 // '1 + (-2'
+		return [...str].filter((item, index) => index < cutoff)
 			.join('')
 	},
 
-	createCheckStrings: function (locationsArr, openParensStr, storePrefixStr) { // [8] // '1 + (-2 +' // '1 + (-2 '
+	createCheckStrings: function (locationsArr, openParensStr, storePrefixStr) {
 		// creates an array of substrings so that
 		// each can be checked for the proper
 		// location of a closing parenthesis
@@ -65,32 +88,33 @@ const PosNeg = {
 		const checkStrings = []
 
 		for (let i = 0; i < locationsArr.length; i++) {
-			checkStr = openParensStr.substring(locationsArr[i], locationsArr[i + 1]) // '1 + (-2 +' // '+'
+			// start at locationsArr[i] + 1 to remove the now-superfluous
+			// space after the last operator in each substring
+			checkStr = openParensStr.substring(locationsArr[i] + 1, locationsArr[i + 1])
 			checkStrings.push(checkStr)
 		}
 
-		return this.findCloseParensLocs(checkStrings, locationsArr, storePrefixStr) // ['+'] // [8] // '1 + (-2 '
+		return this.findCloseParensLocs(checkStrings, locationsArr, storePrefixStr)
 	},
 
-	findCloseParensLocs: function (checkStringsArr, locationsArr, storePrefixStr) { // ['+'] // [8] // '1 + (-2 '
-		// finds the index position in each checkString
-		// where a closing parenthesis needs
-		// to be inserted
+	findCloseParensLocs: function (checkStringsArr, locationsArr, storePrefixStr) {
+		// finds the index position in each checkString where 
+		// a closing parenthesis needs to be inserted
 		const closeParensLocations = []
 		
 		checkStringsArr.forEach(substr => {
 			for (let i = 0; i < substr.length; i++) {
 				if (ops.includes(substr[i])) {
-					closeParensLocations.push(substr.indexOf(substr[i])) // [0]
+					closeParensLocations.push(i)
 					return
 				}
 			}
 		})
 
-		return this.insertCloseParens(closeParensLocations, checkStringsArr, storePrefixStr) // [0] // ['+'] // '1 + (-2 '
+		return this.insertCloseParens(closeParensLocations, checkStringsArr, storePrefixStr)
 	},
 
-	insertCloseParens: function (locationsArr, substringsArr, storePrefixStr) { // [0] // ['+'] // '1 + (-2 '
+	insertCloseParens: function (locationsArr, substringsArr, storePrefixStr) {
 		// takes each substring and inserts the closing
 		// parenthesis then adds it to an array
 		let counter = 0
@@ -98,19 +122,19 @@ const PosNeg = {
 		
 		substringsArr.forEach(substr => {
 			let substrArray = [...substr]
-			substrArray.splice(locationsArr[counter], 0, ') ') // [') ','+']
+			substrArray.splice(locationsArr[counter], 0, ') ')
 			counter++
-			newSubStrings.push(substrArray.join('')) // [') +']
+			newSubStrings.push(substrArray.join(''))
 		})
 
-		return this.createStoreWithParens(storePrefixStr, newSubStrings.join('')) // '1 + (-2 ' // ') +'
+		return this.createStoreWithParens(storePrefixStr, newSubStrings.join(''))
 	},
 
-	createStoreWithParens: (storePrefixStr, storeEndStr) => // '1 + (-2 ' // ') +'
+	createStoreWithParens: (storePrefixStr, storeEndStr) =>
 		// concatenates the string with all the parentheses
 		// now added to the end of the previously
 		// removed portion of the original string
-		storePrefixStr + storeEndStr, // '1 + (-2 ) +'
+		storePrefixStr + storeEndStr,
 
 	addNegativeSign: str => {
 		// takes a string and adds a negative
